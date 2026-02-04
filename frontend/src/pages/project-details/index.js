15745ddfab7f92d4a90cf49186e7198140beb693
Yanis Kara <yanis.kara@edu.esiee.fr>
2026-02-04 16:10:05 +0100

    extract project details into dedicated module

---
+++ frontend/src/pages/project-details/index.js
@@ -0,0 +1,11 @@
+/**
+ * Sous-composants pour ProjectDetailsPage
+ * Fichier d'index pour exporter tous les composants
+ */
+
+export { default as ProjectHeader } from './ProjectHeader';
+export { default as StudentsList } from './StudentsList';
+export { default as ExchangeSection } from './ExchangeSection';
+export { default as GroupPreferencesSection } from './GroupPreferencesSection';
+export { default as AssignmentsSection } from './AssignmentsSection';
+export { default as DestinationForm } from './DestinationForm';
